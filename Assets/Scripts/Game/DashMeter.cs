ccaf85bfbbb52e1669f9dd4dc19427cfd6e23b16
Mirsad Hasic <45371385+Miki-Miki@users.noreply.github.com>
2020-06-04 15:05:03 +0200

    built

--- Assets/Scripts/Game/DashMeter.cs
+++ Assets/Scripts/Game/DashMeter.cs
@@ -11,17 +11,10 @@ public class DashMeter : MonoBehaviour
     {
         dashManager = GameObject.FindGameObjectWithTag("Player").GetComponent<Dash>();
         anim = gameObject.GetComponent<Animator>();
-        Debug.developerConsoleVisible = true;
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.LeftShift)) {
-            Debug.LogError("hasDashed: " + dashManager.hasDash);
-            Debug.LogError("RechargeTime: " + 1/dashManager.timeBeforeNextDash);
-            Debug.LogError("IsDashMeterFull: " + dashManager.dashFull);
-        }
-
         anim.SetBool("hasDashed", dashManager.hasDash);
         anim.SetFloat("RechargeTime", 1/dashManager.timeBeforeNextDash);
         anim.SetBool("IsDashMeterFull", dashManager.dashFull);
